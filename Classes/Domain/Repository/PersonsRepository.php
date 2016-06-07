9ded9aa9fa23000c9d05850f17f703485b679dc1
Manfred Mirsch <mmirsch@googlemail.com>
2016-06-07 13:30:50 +0200

    minor bugfix in PersonsRepository
    filedname correctet

--- Classes/Domain/Repository/PersonsRepository.php
+++ Classes/Domain/Repository/PersonsRepository.php
@@ -4,13 +4,29 @@ namespace HSE\HeTools\Domain\Repository;
 class PersonsRepository extends \TYPO3\CMS\Extbase\Persistence\Repository{
 
 	/**
+	 * Gets single fe_user by username
+	 *
 	 * @param string $username
 	 * @return QueryResultInterface|array
 	 */
 	public function findByUsername($username)	{
 		/** @var \TYPO3\CMS\Extbase\Persistence\QueryInterface $query */
 		$query = $this->createQuery();
-		$query->like('username.title', $username);
+		$query->like('feuser.username', $username);
+		$queryResult = $query->execute();
+		return $queryResult;
+	}
+
+	/**
+	 * Gets single fe_user by email
+	 *
+	 * @param string $email
+	 * @return QueryResultInterface|array
+	 */
+	public function findByEmail($email)	{
+		/** @var \TYPO3\CMS\Extbase\Persistence\QueryInterface $query */
+		$query = $this->createQuery();
+		$query->like('feuser.email', $email);
 		$queryResult = $query->execute();
 		return $queryResult;
 	}
