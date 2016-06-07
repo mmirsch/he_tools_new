002befca1f3c28c52dc29a7f0bbdf6f8abaa0faa
Manfred Mirsch <mmirsch@googlemail.com>
2016-06-07 14:00:51 +0200

    fix sync problems

--- Classes/Domain/Repository/PersonsRepository.php
+++ Classes/Domain/Repository/PersonsRepository.php
@@ -3,33 +3,7 @@ namespace HSE\HeTools\Domain\Repository;
 
 class PersonsRepository extends \TYPO3\CMS\Extbase\Persistence\Repository{
 
-	/**
-	 * Gets single fe_user by username
-	 *
-	 * @param string $username
-	 * @return QueryResultInterface|array
-	 */
-	public function findByUsername($username)	{
-		/** @var \TYPO3\CMS\Extbase\Persistence\QueryInterface $query */
-		$query = $this->createQuery();
-		$query->like('feuser.username', $username);
-		$queryResult = $query->execute();
-		return $queryResult;
-	}
 
-	/**
-	 * Gets single fe_user by email
-	 *
-	 * @param string $email
-	 * @return QueryResultInterface|array
-	 */
-	public function findByEmail($email)	{
-		/** @var \TYPO3\CMS\Extbase\Persistence\QueryInterface $query */
-		$query = $this->createQuery();
-		$query->like('feuser.email', $email);
-		$queryResult = $query->execute();
-		return $queryResult;
-	}
 
 
 }
