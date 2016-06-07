513339924486a4dc80c46be680682df93ca447a1
Manfred Mirsch <mmirsch@googlemail.com>
2016-06-07 14:28:31 +0200

    Search functions for repository

--- Classes/Domain/Repository/PersonsRepository.php
+++ Classes/Domain/Repository/PersonsRepository.php
@@ -6,6 +6,35 @@ use HSE\HeTools\Service\Persons\Import\PersImport;
 
 class PersonsRepository extends \TYPO3\CMS\Extbase\Persistence\Repository{
 
+    /**
+     * Gets single fe_user by username
+     *
+     * @param string $username
+     * @return QueryResultInterface|array
+     */
+    public function findByUsername($username)	{
+        /** @var \TYPO3\CMS\Extbase\Persistence\QueryInterface $query */
+        $query = $this->createQuery();
+        $query->like('feuser.username', $username);
+        $queryResult = $query->execute();
+        return $queryResult;
+    }
+
+    /**
+     * Gets single fe_user by email
+     *
+     * @param string $email
+     * @return QueryResultInterface|array
+     */
+    public function findByEmail($email)	{
+        /** @var \TYPO3\CMS\Extbase\Persistence\QueryInterface $query */
+        $query = $this->createQuery();
+        $query->like('feuser.email', $email);
+        $queryResult = $query->execute();
+        return $queryResult;
+    }
+
+
     public function createCsvArray(){
 
         $extPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('he_tools');
