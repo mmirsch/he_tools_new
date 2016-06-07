62cd2b9865c19c22cea78961bf9d755d7ef99b50
Manfred Mirsch <mmirsch@googlemail.com>
2016-06-07 12:55:48 +0200

    Add repository search function

--- Classes/Domain/Repository/PersonsRepository.php
+++ Classes/Domain/Repository/PersonsRepository.php
@@ -3,6 +3,19 @@ namespace HSE\HeTools\Domain\Repository;
 
 class PersonsRepository extends \TYPO3\CMS\Extbase\Persistence\Repository{
 
+	/**
+	 * @param string $username
+	 * @return QueryResultInterface|array
+	 */
+	public function findByUsername($username)	{
+		/** @var \TYPO3\CMS\Extbase\Persistence\QueryInterface $query */
+		$query = $this->createQuery();
+		$query->like('username.title', $username);
+		$queryResult = $query->execute();
+		return $queryResult;
+	}
+
+
 }
 
 ?>
