6522da5740ae27c969cba0ab9e28be3d9588afab
Manfred Mirsch <mmirsch@googlemail.com>
2016-06-13 13:54:52 +0200

    Logo and german language
    changed logo and added german language files

--- ext_tables.php
+++ ext_tables.php
@@ -31,7 +31,7 @@ if (TYPO3_MODE === 'BE') {
 		),
 		array(
 			'access' => 'user,group',
-			'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
+			'icon'   => 'EXT:' . $_EXTKEY . '/Ressources/Public/Icons/he_tools.svg',
 			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_hetools.xlf',
 		)
 	);
