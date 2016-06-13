685af71f668873f393136deca38e307f26e45871
Manfred Mirsch <mmirsch@googlemail.com>
2016-06-13 14:22:39 +0200

    Revert "Delete PersonsRepository.php"
    This reverts commit 64773a0b121270b58770f7fdd352e8f6e4707fc5.

---
+++ Classes/Domain/Repository/PersonsRepository.php
@@ -0,0 +1,8 @@
+<?php
+namespace HSE\HeTools\Domain\Repository;
+use HSE\HeTools\Domain\Model\Persons;
+use HSE\HeTools\Service\Persons\Import\PersImport;
+use HSE\HeTools\Utility\ExtensionUtility;
+class PersonsRepository extends \TYPO3\CMS\Extbase\Persistence\Repository{
+
+}
