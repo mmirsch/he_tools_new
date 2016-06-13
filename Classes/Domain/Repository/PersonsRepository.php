64773a0b121270b58770f7fdd352e8f6e4707fc5
Manfred Mirsch <mmirsch@googlemail.com>
2016-06-13 14:10:47 +0200

    Delete PersonsRepository.php

--- Classes/Domain/Repository/PersonsRepository.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace HSE\HeTools\Domain\Repository;
-use HSE\HeTools\Domain\Model\Persons;
-use HSE\HeTools\Service\Persons\Import\PersImport;
-use HSE\HeTools\Utility\ExtensionUtility;
-class PersonsRepository extends \TYPO3\CMS\Extbase\Persistence\Repository{
-
-}
